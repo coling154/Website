8397e4b16187863e0bded700def4b76c1b73056c
Colin <gasiewca@clarkson.edu>
2022-04-13 19:27:22 -0400

    did some node work

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
-var http = require('http');
-var fs =require('fs');
-var url=require('url');
-var hostname='127.0.0.1';
+const http = require('http');
+const fs =require('fs');
+const url=require('url');
+const hostname='127.0.0.1';
 const port=5000;
 //http://localhost:5000/home.html
 http.createServer(function (req, res) {
     console.log(req.url)
     var q =url.parse(req.url, true);
     var filename="."+q.pathname;
-    fs.readFile('home.html',function(err, data){
+    fs.readFile(filename,function(err, data){
         if(err){
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end("404 Not Found");
